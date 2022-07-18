3519feef8067ae0c8c1c9817842b9b88dbb1d72a
MelanieRosi <MelanieRosi25@gmail.com>
2022-07-18 19:23:16 -0300

    avance de header y footer y mi detalle de producto

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require ('express');
+const path = require('path');
+const app = express();
+const port = 3000;
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/',(req,res) => res.sendFile(path.resolve(__dirname,'views','home.html')));
+app.get('/detalle',(req,res) => res.sendFile(path.resolve(__dirname,'views','productDetail.html')));
+
+app.listen(port, () => console.log(`Se levantó con exito el servidor en http://localhost:${port}`));
